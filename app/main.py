37f15df1ff1838bf1b947d43954679bfc9f4a89f
A.S.Ayon <animeshsingha-2023816008@cs.du.ac.bd>
2026-08-30 21:09:57 +0800

    Fix CORS configuration by removing comment

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://frontend-shop-mauve.vercel.app",  # ← add this
+        "https://frontend-shop-mauve.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
