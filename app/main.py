e6de06cc6cef84158db5ec9141115044501a65d1
A.S.Ayon <animeshsingha-2023816008@cs.du.ac.bd>
2026-08-30 22:43:27 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://frontend-shop-mauve.vercel.app",
+    
     "https://3000-ipyd9rbapz9vqtoizhoug-e611ea41.us4.manus.computer",
     ],
     allow_credentials=True,
